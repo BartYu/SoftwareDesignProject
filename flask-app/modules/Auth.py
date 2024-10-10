0bbfde206fb6d362a9c1a604658357520d02b0bf
zayyrahman <Zayedrahman888@gmail.com>
2024-10-10 16:26:54 -0500

    Revert "changes made to ad history"
    This reverts commit 7ff7fcef6699197266b64f8ee332c21dd38c1ca1.

--- flask-app/modules/Auth.py
+++ flask-app/modules/Auth.py
@@ -1,10 +1,14 @@
 from functools import wraps
 from flask import session, jsonify
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print("Current session:", session.get("user_id"))
         if "user_id" not in session:
-            return jsonify({"msg": "User not logged in."}), 403
+            print("Session not found.")
+            return jsonify({"redirect": "No session."}), 401
         return f(*args, **kwargs)
+
     return decorated_function
--- flask-app/modules/Profile.py
+++ flask-app/modules/Profile.py
@@ -7,45 +7,100 @@ import re
 import App
 
 profile_bp = Blueprint("profile", __name__)
+
 ma = Marshmallow(App)
 
-class DatesFormat(fields.Date):
+
+class datesFormat(fields.Date):
     def _deserialize(self, value, attr, data, **kwargs):
+        # print("Dates received: ", value)
         try:
             if isinstance(value, int):
                 timestamp = value / 1000
                 date_value = datetime.fromtimestamp(timestamp).date()
             else:
                 date_value = datetime.strptime(value, "%m/%d/%Y").date()
+
             return date_value
         except ValueError:
             raise ValidationError("Invalid date format. Expected MM/DD/YYYY.")
 
+
 class ProfileSchema(ma.Schema):
-    name = fields.String(required=True, validate=lambda s: 1 <= len(s) <= 50)
-    address1 = fields.String(required=True, validate=lambda s: 1 <= len(s) <= 100)
+    name = fields.String(
+        required=True,
+        validate=lambda s: 1 <= len(s) <= 50,
+        error_messages={
+            "validator_failed": "Full name must be between 1 - 50 characters.",
+        },
+    )
+    address1 = fields.String(
+        required=True,
+        validate=lambda s: 1 <= len(s) <= 100,
+        error_messages={
+            "validator_failed": "Address must be between 1 - 100 characters.",
+        },
+    )
     address2 = fields.String(allow_none=True, validate=lambda s: len(s) <= 100)
-    city = fields.String(required=True, validate=lambda s: 1 <= len(s) <= 100)
-    state = fields.String(required=True, validate=lambda s: len(s) == 2)
-    zipcode = fields.String(required=True, validate=lambda s: re.match(r"^\d{5}(-\d{4})?$", s) is not None)
-    skills = fields.List(fields.String(), required=True, validate=lambda s: len(s) > 0)
+    city = fields.String(
+        required=True,
+        validate=lambda s: 1 <= len(s) <= 100,
+        error_messages={
+            "validator_failed": "Enter a valid city",
+        },
+    )
+    state = fields.String(
+        required=True,
+        validate=lambda s: len(s) == 2,
+        error_messages={
+            "validator_failed": "Select your State.",
+        },
+    )
+    zipcode = fields.String(
+        required=True,
+        validate=lambda s: re.match(r"^\d{5}(-\d{4})?$", s) is not None,
+        error_messages={
+            "required": "Enter a valid zip code.",
+            "validator_failed": "Zip code must be in the format XXXXX or XXXXX-XXXX.",
+        },
+    )
+    skills = fields.List(
+        fields.String(),
+        required=True,
+        validate=lambda s: len(s) > 0,
+        error_messages={
+            "validator_failed": "Select at least 1 skill.",
+        },
+    )
     preferences = fields.String(allow_none=True, validate=lambda s: len(s) <= 300)
-    dates = fields.List(DatesFormat(), required=True, validate=lambda s: len(s) > 0)
+    dates = fields.List(
+        datesFormat(),
+        required=True,
+        validate=lambda s: len(s) > 0,
+        error_messages={
+            "validator_failed": "Select at least a date.",
+        },
+    )
+
 
 profile_schema = ProfileSchema()
 user_profile = {}
+
 
 @profile_bp.route("/profile", methods=["GET", "PUT"])
 @login_required
 def profile():
     user_id = session.get("user_id")
+    # print("Profile session:", session.get("user_id"))
 
     if request.method == "PUT":
+        # print("Request JSON:", request.json)
         try:
             data = profile_schema.load(request.json)
         except ValidationError as error:
             return jsonify({"errors": error.messages}), 400
 
+        # To be sent to database
         user_profile[user_id] = {
             "name": data.get("name"),
             "address1": data.get("address1"),
@@ -61,6 +116,7 @@ def profile():
 
     if request.method == "GET":
         profile_info = user_profile.get(user_id)
+        print("Profile:", profile_info)
         if profile_info:
             return jsonify(profile_info), 200
         return jsonify({"msg": "Profile not found."}), 404
