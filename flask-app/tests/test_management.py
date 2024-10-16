32ff51e5be1ea1b31941738543885ef2a694b0fe
Anh Pham <114327650+anh15004@users.noreply.github.com>
2024-10-15 22:46:56 -0500

    some basic util functions that were recommended for writing test case

--- flask-app/tests/test_management.py
+++ flask-app/tests/test_management.py
@@ -4,6 +4,29 @@ from datetime import datetime
 EVENT_PATH = "/event/management"
 fields_to_check = ['name', 'description', 'location', 'skills', 'urgency', 'date'] 
 
+# Convert dates to test methods
+def convert_date(date_string):
+    dt_object = datetime.strptime(date_string, '%a, %d %b %Y %H:%M:%S %Z')
+    return dt_object.strftime('%m/%d/%Y')
+
+def test_get_empty_event(auth_client):
+    response = auth_client.get(EVENT_PATH)
+    assert response.status_code == 404
+
+def test_update_event(auth_client, event):
+    response = auth_client.post(EVENT_PATH, json=event)
+    assert response.status_code == 200
+    assert response.json == {"msg": "Event saved successfully!"}
+
+    response = auth_client.get(EVENT_PATH)
+    assert response.status_code == 200
+    for field in fields_to_check:
+        assert response.json[field] == event[field], f"{field.capitalize()} does not match!"
+def test_update_invalid_event(auth_client, invalid_event):
+    response = auth_client.put(EVENT_PATH, json=invalid_event)
+    assert response.status_code == 400
+    assert "errors" in response.json
+
 # @pytest.mark.parametrize("updated_event_info", [
 #     {
 #         "name": "Computer Science Fundraising",
