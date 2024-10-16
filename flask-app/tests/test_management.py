5e7457a3f366ea258ae6f24e41738fd23db55f06
Anh Pham <114327650+anh15004@users.noreply.github.com>
2024-10-15 22:47:33 -0500

    some more test cases for management - for PUT method

--- flask-app/tests/test_management.py
+++ flask-app/tests/test_management.py
@@ -27,13 +27,23 @@ def test_update_invalid_event(auth_client, invalid_event):
     assert response.status_code == 400
     assert "errors" in response.json
 
-# @pytest.mark.parametrize("updated_event_info", [
-#     {
-#         "name": "Computer Science Fundraising",
-#         "description": "Fundraising and workshop",
-#         "location": "9999 Hazen St, Sugar Land, Texas 77899",
-#         "skills": ["Adaptive", "Problem-Solving", "Teamwork"],
-#         "urgency": "Medium",
-#         "date": "10/11/2024",
-#     }
-# ])
+@pytest.mark.parametrize("updated_event_info", [
+    {
+        "name": "Computer Science Fundraising",
+        "description": "Fundraising and workshop",
+        "location": "9999 Hazen St, Sugar Land, Texas 77899",
+        "skills": ["Adaptive", "Problem-Solving", "Teamwork"],
+        "urgency": "Medium",
+        "date": "10/11/2024",
+    }
+])
+def test_get_updated_event_info(auth_client, updated_event_info):
+    auth_client.put(EVENT_PATH, json=updated_event_info)
+
+    response = auth_client.get(EVENT_PATH)
+    assert response.status_code == 200
+    for field in fields_to_check:
+        assert response.json[field] == updated_event_info[field], f"{field.capitalize()} does not match!"
+
+    converted_dates = [convert_date(date) for date in response.json['dates']]
+    assert converted_dates == updated_event_info['dates'], "Dates do not match!"
