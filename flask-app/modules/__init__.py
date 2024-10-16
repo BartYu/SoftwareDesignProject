7c09b2323e69a59fd1e111801d5ecfb91e956908
Anh Pham <114327650+anh15004@users.noreply.github.com>
2024-10-15 22:42:00 -0500

    added basic backend logic for Management page

--- flask-app/modules/__init__.py
+++ flask-app/modules/__init__.py
@@ -2,6 +2,8 @@ def register_routes(app):
     # Import modules / classes here
     from .Login import login_bp
     from .Profile import profile_bp
+    from .Management import management_bp
 
     app.register_blueprint(login_bp, url_prefix="/auth")
     app.register_blueprint(profile_bp, url_prefix="/user")
+    app.register_blueprint(management_bp, url_prefix="/event")
